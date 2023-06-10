4c2b5ec39132863d67eab37dcbb7a74f1d3ada2d
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-10 19:08:30 -0300

    Se hace uso del componente BannerMain.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 import Menu from "./views/Menu";
 import Footer from "./views/Footer";
 import ContenedorRecetas from "./components/ContenedorRecetas";
+import BannerMain from "./components/BannerMain";
 
 function App() {
   return (
     <>
       <Menu></Menu>
       <section className="mainPage">
+        <BannerMain></BannerMain>
         <ContenedorRecetas />
       </section>
       <Footer></Footer>
