69d3d04de22f7d4e6383706ca777190ff628b3b4
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-10 19:09:17 -0300

    Se agrega el maquetado basico del banner para celular

--- src/components/BannerMain.jsx
+++ src/components/BannerMain.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import { Button, Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 const BannerMain = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <section className="banner text-light text-center mb-5">
+      <Image src="../src/assets/banner.png" className="w-100"></Image>
+      <h3>Sabores irresistibles</h3>
+      <p>
+        recetas deliciosas y faciles de aprender, ¡inspirate en nuestra cocina!
+      </p>
+      <Button className="w-75 btn-banner">
+        Empeza a cocinar <i class="bi bi-arrow-right"></i>
+      </Button>
+    </section>
+  );
 };
 
 export default BannerMain;
