ffbdd80b26d98e26d60d6d6b4f3c89c83ff8bd19
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-10 20:05:02 -0300

    Se corrige el maquetado del componente BannerMain.jsx

--- src/components/BannerMain.jsx
+++ src/components/BannerMain.jsx
@@ -1,17 +1,22 @@
-import { Button, Image } from "react-bootstrap";
+import { Button, Container, Image } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 const BannerMain = () => {
   return (
-    <section className="banner text-light text-center mb-5">
-      <Image src="../src/assets/banner.png" className="w-100"></Image>
-      <h3>Sabores irresistibles</h3>
-      <p>
-        recetas deliciosas y faciles de aprender, ¡inspirate en nuestra cocina!
-      </p>
-      <Button className="w-75 btn-banner">
-        Empeza a cocinar <i class="bi bi-arrow-right"></i>
-      </Button>
+    <section className="banner text-light text-centermx-0 row align-items-center mb-5">
+      <div className="col-md-6 banner-image">
+        <Image src="../src/assets/banner.png" className="w-100"></Image>
+      </div>
+      <div className="col-md-6">
+        <h3>Sabores irresistibles</h3>
+        <p>
+          recetas deliciosas y faciles de aprender, ¡inspirate en nuestra
+          cocina!
+        </p>
+        <Button className="w-100 btn-banner p-3">
+          Empeza a cocinar <i className="bi bi-arrow-right"></i>
+        </Button>
+      </div>
     </section>
   );
 };
