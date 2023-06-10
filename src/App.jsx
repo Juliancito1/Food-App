73cc960b648ce987bef12f0b7eefa9eaf15d5a1b
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-10 20:03:46 -0300

    Se coloca el componente BannerMain.jsx dentro de un container para corregir unos errores de margin

--- src/App.jsx
+++ src/App.jsx
@@ -4,13 +4,16 @@ import Menu from "./views/Menu";
 import Footer from "./views/Footer";
 import ContenedorRecetas from "./components/ContenedorRecetas";
 import BannerMain from "./components/BannerMain";
+import { Container } from "react-bootstrap";
 
 function App() {
   return (
     <>
       <Menu></Menu>
       <section className="mainPage">
+        <Container>
         <BannerMain></BannerMain>
+        </Container>
         <ContenedorRecetas />
       </section>
       <Footer></Footer>
