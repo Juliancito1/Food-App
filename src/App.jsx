d03a333de84a26fd2b5bfa41848d3133870317f7
Julian <julianser@hotmail.com.ar>
2023-06-10 19:35:07 -0300

    Se creo el componente CardDestacados y se agrego la clase textoVerde en css

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Menu from "./views/Menu";
 import Footer from "./views/Footer";
 import ContenedorRecetas from "./components/ContenedorRecetas";
+import CardDestacados from "./components/CardDestacados";
 
 function App() {
   return (
     <>
       <Menu></Menu>
       <section className="mainPage">
+        <CardDestacados></CardDestacados>
         <ContenedorRecetas />
       </section>
       <Footer></Footer>
