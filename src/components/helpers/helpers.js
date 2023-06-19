3e6c3b963368f548441da68de8753dc1eb012de5
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-19 16:58:31 -0300

    Se agrega la funcion obtenerRecetas

--- src/components/helpers/helpers.js
+++ src/components/helpers/helpers.js
@@ -1,6 +1,16 @@
 const URL_USUARIO = import.meta.env.VITE_API_USUARIO;
 const URL_RECETAS = import.meta.env.VITE_API_RECETAS;
 
+export const obtenerProductos = async () =>{
+    try{
+        const respuesta = await fetch(URL_RECETAS);
+        const listaRecetas = await respuesta.json();
+        return listaRecetas;
+    }catch(e){
+        console.log(e); 
+    }
+}
+
 export const crearReceta = async (receta) => {
     try {
         const respuesta = await fetch(URL_RECETAS,{
