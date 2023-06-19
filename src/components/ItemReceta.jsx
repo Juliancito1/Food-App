4493c6c838c836d12865af64a1eddb7de28fdb8e
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-06-19 17:01:23 -0300

    Se hace uso de la receta recibid a travez de props para dibuujar cada dato correspondiente del producto

--- src/components/ItemReceta.jsx
+++ src/components/ItemReceta.jsx
@@ -1,10 +1,10 @@
 import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-const ItemReceta = () => {
+const ItemReceta = ({receta, setRecetas}) => {
     return (
         <tr>
-          <td>1</td>
-          <td>Milanesa con Papas Fritas</td>
+          <td>{receta.id}</td>
+          <td>{receta.nombreReceta}</td>
           <td><Link className='btn btn-outline-light textoVerde mb-2 mb-md-0' to={'/administrador/editar'}>Editar</Link>
           <Button className='ms-md-2 fondoVerde text-black' variant='outline-light'>Borrar</Button></td>
         </tr>
