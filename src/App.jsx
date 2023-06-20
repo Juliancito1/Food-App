ad62a69548cbdae9d216ab1c41325dd911ed5471
Julian <julianser@hotmail.com.ar>
2023-06-20 16:32:44 -0300

    Se agregaron las rutas protegidas para administrador

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,14 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Menu from "./views/Menu";
 import Footer from "./views/Footer";
-import Administrador from "./components/Administrador";
 import Login from "./components/Login";
 import Inicio from "./components/Inicio";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import FormularioReceta from "./components/FormularioReceta";
 import { useState } from "react";
-
+import RutasAdministrador from "./routes/RutasAdministrador";
+import RutasProtegidas from "./routes/RutasProtegidas"
 function App() {
-  const usuarioStorage = JSON.parse(sessionStorage.getItem("user")) || {};
+  const usuarioStorage = JSON.parse(sessionStorage.getItem("user")) || null;
   const [usuario, setUsuario] = useState(usuarioStorage);
 
   return (
@@ -20,27 +19,13 @@ function App() {
         <Route exact path="/" element={<Inicio></Inicio>}></Route>
         <Route
           exact
-          path="/administrador"
-          element={<Administrador></Administrador>}
-        ></Route>
-        <Route
-          exact
-          path="/administrador/crear"
-          element={<FormularioReceta></FormularioReceta>}
-        ></Route>
-        <Route
-          exact
-          path="/administrador/editar/:id"
-          element={<FormularioReceta></FormularioReceta>}
-        ></Route>
-        <Route
-          exact
           path="/login"
           element={<Login setUsuario={setUsuario}></Login>}
         ></Route>
-        {/* <FormularioReceta></FormularioReceta>
-        <DetalleReceta></DetalleReceta> */}
-        {/* <Login /> */}
+        <Route exact path="/administrador/*" element={
+        <RutasProtegidas>
+          <RutasAdministrador></RutasAdministrador>
+        </RutasProtegidas>}></Route>
       </Routes>
       <Footer></Footer>
     </BrowserRouter>
