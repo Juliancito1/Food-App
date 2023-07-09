dd3c671587e8492496a53f832a8474aa5044c076
Federico Ledesma <federicolucianoledesma@gmail.com>
2023-07-09 15:26:10 -0300

    Se elimina la tabla usuarios de administrador

--- src/components/Administrador.jsx
+++ src/components/Administrador.jsx
@@ -1,6 +1,5 @@
-import { Table, Button, Container } from "react-bootstrap";
+import { Table, Container } from "react-bootstrap";
 import ItemReceta from "./ItemReceta";
-import ItemUsuario from "./ItemUsuario";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { obtenerRecetas } from "./helpers/helpers";
@@ -46,30 +45,6 @@ const Administrador = () => {
           </tbody>
         </Table>
       </section>
-
-      <div className="d-flex justify-content-between align-items-center mt-5">
-        <h1 className="display-4 text-center text-light">Usuarios</h1>
-        <Button className="btn text-dark fondoVerde" variant="outline-success">
-          Agregar
-        </Button>
-      </div>
-      <hr></hr>
-      <div className="table-responsive">
-        <Table striped bordered hover className="table-dark">
-          <thead>
-            <tr>
-              <th>Id</th>
-              <th>Usuario</th>
-              <th>Email</th>
-              <th>Rol</th>
-              <th>Opciones</th>
-            </tr>
-          </thead>
-          <tbody>
-            <ItemUsuario></ItemUsuario>
-          </tbody>
-        </Table>
-      </div>
     </Container>
   );
 };
